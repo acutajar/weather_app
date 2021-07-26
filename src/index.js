c98a13608eda92a4d2328fbcf75ca58d8de521a8
Alec Cutajar <aleccutajar@gmail.com>
2021-07-26 15:54:01 -0400

    page layout and styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./style.css";
 import { getForecast } from "./api_call";
+import { format, fromUnixTime } from "date-fns";
 
 async function test() {
   const weatherData = await getForecast();
-  console.log(weatherData.current);
+  console.log(weatherData);
   console.log(weatherData.daily);
   console.log(weatherData.hourly);
+  console.log(format(new Date(fromUnixTime(weatherData.hourly[1].dt)), "h aa"));
 }
 
 test();
