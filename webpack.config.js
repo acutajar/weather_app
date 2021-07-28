c11199b46287f7946e14acf903adc7eb60bef41e
Alec Cutajar <aleccutajar@gmail.com>
2021-07-28 14:03:58 -0400

    remove font loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,6 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
     ],
   },
 };
